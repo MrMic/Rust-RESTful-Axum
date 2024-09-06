3d306c49b519d11749b5e007c897a048220ca1fe
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-06 14:30:23 +0200

    feat: Create Book Struct / Data Store / Use DS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+
+mod book;
+mod data;
+
+/// To access data, create a thread, spawn it, then get the lock.
+/// When you're done, then join the thread with its parent thread.
+async fn print_data() {
+    thread::spawn(move || {
+        let data = data::DATA.lock().unwrap();
+        println!("{:?}", data);
+    })
+    .join()
+    .unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    print_data().await;
 }
